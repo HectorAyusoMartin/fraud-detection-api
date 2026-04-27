ce397ea3a265e3633428da293b89df57a8bc151c
Hector <hector.ayuso.martin@gmail.com>
2026-04-27 10:57:27 +0200

    Refactor en settings configuration

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
 
-    app_name : str
-    app_version : str
-    debug : bool
-    host : str
-    port : int
+    model_config =  SettingsConfigDict(env_file='.env')
 
-    class Config:
-        env_file = ".env"
+    app_name: str
+    app_version: str
+    debug: bool
+    host: str
+    port: int
+
 
 settings = Settings()
+
+
+
