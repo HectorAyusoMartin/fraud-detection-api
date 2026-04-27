eb36c22ea7fedaadf553b72b517f94e1c9435905
Hector <hector.ayuso.martin@gmail.com>
2026-04-27 11:42:30 +0200

    Creado modelo Transaction y tabla transanctions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.db.base import Base
+from app.db.session import engine
+from app.models.transaction import Transaction
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title=settings.app_name,
